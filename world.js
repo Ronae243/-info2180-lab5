a1dbf8a09c6f666a291e63c7b0ee43e85f454492
Ronae Johnson <ronae2004@gmail.com>
2021-11-03 13:05:02 -0500

    world.js ver 1.0

---
+++ world.js
@@ -0,0 +1,36 @@
+/* Add your JavaScript to this file */
+
+function searchbtn(){
+    var subbutton = document.querySelector("button");
+    console.log(subbutton);
+    subbutton.addEventListener("click", function(event) {
+        suphero();
+        event.preventDefault();
+    });
+}
+
+function sanitizeString(str){
+    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
+    return str.trim();
+}
+
+function suphero(){
+
+    var userent = document.querySelector("input").value;
+    var sanUser = sanitizeString(userent);
+    //var strResult = document.getElementById("result");
+    var http = new XMLHttpRequest();
+    var url = "http://localhost:8080/-info2180-lab5/world.php?country=";
+    http.onreadystatechange = function(){
+        if(http.readyState == XMLHttpRequest.DONE && http.status ==200){
+               alert(http.responseText)
+        }
+    }
+    http.open('GET', url+sanUser, true);
+    http.send();  
+}
+
+
+window.addEventListener("DOMContentLoaded",function(){
+    searchbtn();
+})
