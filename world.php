566ca3a5478fd5891e42aafb22838c5a33885901
Ronae Johnson <ronae2004@gmail.com>
2021-11-05 12:43:35 -0500

    world.php 3.0

--- world.php
+++ world.php
@@ -7,19 +7,17 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
-$country = $_GET['country'];
-$cities = $_GET['context'];
-
+$country = (isset ($_GET['country']) ==TRUE) ? $_GET['country']:"";
+$cities = (isset($_GET['context'] )==TRUE) ? $_GET['context']: "";
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-//$stmt = $conn->query("SELECT * FROM countries");
+
 $userent = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-$userent_cities= $conn->query("SELECT cities.name, cities.district, cities.population FROM cities Join countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$cities%'");
-//$userent_cities = $conn->query("SELECT * FROM cities WHERE name LIKE '%$cities%'");
+$userent_citiesby_country= $conn->query("SELECT cities.name, cities.district, cities.population FROM cities Join countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$cities%'");
+
 
 $results = $userent->fetchAll(PDO::FETCH_ASSOC);
-$results_c = $userent_cities->fetchAll(PDO::FETCH_ASSOC);
-
+$results_c_c = $userent_citiesby_country->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
@@ -56,14 +54,14 @@ if (isset($_GET['country'])){
 <?php if ($state == "false"): ?>
   <table>
    <thead>
-     <tr>
+     <tr1>
          <th scope="col"><?= "Name" ?></th>
          <th scope="col"><?= "District"?></th>
          <th scope="col"><?= "Population" ?></th>
-     </tr>
+     </tr1>
    </thead>
    <tbody>
-     <?php foreach ($results_c as $ow): ?>
+     <?php foreach ($results_c_c as $ow): ?>
        <tr>
          <td><?= $ow['name']; ?></td> 
          <td><?= $ow['district']; ?></td> 
